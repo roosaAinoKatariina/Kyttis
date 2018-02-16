87cef485374ffc08e41a6a834057131f356b5ce6
X230 <X230@130.232.62.63>
2018-02-16 10:45:51 +0200

    tunnus ja salasana lisätty

--- Museojunat/src/varaus/model/User.java
+++ Museojunat/src/varaus/model/User.java
@@ -12,7 +12,8 @@ package varaus.model;
 	/**
 	 * Model class for a User.
 	 *
-	 * @author Marco Jakob
+	 * Nyt on paljon infoa, tuskin tarvitaan kaikkea.
+	 *
 	 */
 	public class User {
 
@@ -22,21 +23,51 @@ package varaus.model;
 	    private final IntegerProperty postalCode;
 	    private final StringProperty city;
 	    private final ObjectProperty<LocalDate> birthday;
+	    
+	    private String tunnus;
+	    private String salasana; 
+	    private boolean yp; // ylläpitäjä vai ei
 
 	    /**
 	     * Default constructor.
 	     */
 	    public User() {
 	        this(null, null);
+	        tunnus = " ";
+	        salasana = "0";
+	        yp = false;
 	    }
 
+	    /**
+	     * Konstruktori, jossa meille aluksi oleelliset
+	     * Luo tavallisen käyttäjän
+	     * ei vielä osaa tarkistaa, onko tunnus jo käytössä
+	     * salasana2 lisäksi, niin voisi varmistaa et salasana tulee kaksi kertaa..?
+	     * @param tunnus
+	     * @param salasana
+	     */
+	    public User(String tunnus, String salasana){
+	        this.tunnus = tunnus;
+	        this.salasana = salasana;
+	        yp = false;
+	        
+	        this.firstName = new SimpleStringProperty("Kalle");
+	        this.lastName = new SimpleStringProperty("Tavis");
+	        	
+	        this.street = new SimpleStringProperty("some street");
+	        this.postalCode = new SimpleIntegerProperty(1234);
+	        this.city = new SimpleStringProperty("some city");
+	        this.birthday = new SimpleObjectProperty<LocalDate>(LocalDate.of(1999, 2, 21));
+	    }
+	    
+	    
 	    /**
 	     * Constructor with some initial data.
 	     * 
 	     * @param firstName
 	     * @param lastName
 	     */
-	    public User(String firstName, String lastName) {
+	    public User(String firstName, String lastName, String tunnus) {
 	        this.firstName = new SimpleStringProperty(firstName);
 	        this.lastName = new SimpleStringProperty(lastName);
 
@@ -46,6 +77,8 @@ package varaus.model;
 	        this.city = new SimpleStringProperty("some city");
 	        this.birthday = new SimpleObjectProperty<LocalDate>(LocalDate.of(1999, 2, 21));
 	    }
+	    
+	    //---------------------------------------- Getterit ja setterit 
 
 	    public String getFirstName() {
 	        return firstName.get();
@@ -122,5 +155,3 @@ package varaus.model;
 	
 	
 	
-	
-}
